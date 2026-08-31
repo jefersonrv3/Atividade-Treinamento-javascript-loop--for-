f67c7a794520eaeca3f28dcf2d5f3abb524ea581
jefersonrv3 <jefersonrv33@gmail.com>
2026-08-30 22:39:44 -0300

    commentando

--- script.js
+++ script.js
@@ -22,13 +22,13 @@ const contatos = [{ //isso é um ARRAy que armazena as informações dos nossos 
 function botao() { //essa é a função atrelada ao onclick do botap isso é oque ele irá fazer ao ser clicado
     let encontrou = false //variável que irá mostrar texto na tela no <P> caso o resultado d toda a função do botao retorne sem nenhum match ou resultado idêntico ao esperado dentro do array
     let nomeDigitado = input.value      //variavel onde o usuario digita no input e o texto é armazenado nela pra rodar a função do botao
-    for (let i = 0; i < contatos.length; i++) {                //aqui é o loop for onde (i=0 )por padrão e
-        if (nomeDigitado.toLowerCase() == contatos[i].name.toLowerCase()) { //
-            p.innerHTML = `Contato encontrado: ${contatos[i].name + contatos[i].Number}` //altera <p> com o resultado da busca refinada
+    for (let i = 0; i < contatos.length; i++) {                //aqui é o loop for, onde (i=0 )por padrão e i < menor que contatos.lenght ou seja extensão do array contatos(quantidade de instãncias de dados nele, i++ = adicionar mais 1 ou seja rodará um a um até achar mas em ordem descrescente e se acabar sem achar ele para e retorna false)
+        if (nomeDigitado.toLowerCase() == contatos[i].name.toLowerCase()) { // se nome digitado (tolowercase retira o case sensitive dando como identico letras digitadas minusculas ou maiusculas for idêntico à alguma das instancias no array retornará true )
+            p.innerHTML = `Contato encontrado: ${contatos[i].name + contatos[i].Number}` //altera <p> com o resultado da busca refinada que resultou em true, e exibe uma string template atrelada ao elemento <p> na tela
             encontrou = true
         }
-        if (encontrou === false) {
-            p.innerHTML = "contato não encontrado, tente novamente"
+        if (encontrou === false) {  //caso o loop for retorne falso ele roda innerhtml modificado
+            p.innerHTML = "contato não encontrado, tente novamente"  //inner html modificado mostra string dizendo que o loop nao deu nenhum match
         }
     }
 
